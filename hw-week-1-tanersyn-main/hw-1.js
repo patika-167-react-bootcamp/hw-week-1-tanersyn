cd60d422c52cd1eba255578c4a4846bcc04dbad0
Taner Sayın <50462952+tanersyn@users.noreply.github.com>
2022-02-11 16:01:12 +0300

    Add files via upload

---
+++ hw-week-1-tanersyn-main/hw-1.js
@@ -0,0 +1,23 @@
+const folders = [
+    {
+      id: 5,
+      name: 'Klasör 1',
+      files: [
+        { id: 17, name: 'profil.jpg' },
+        { id: 18, name: 'manzara.jpg'},
+      ],
+    },
+    {
+      id: 6,
+      name: 'Klasör 2',
+      files: [
+        { id: 21, name: 'foto.png' },
+        { id: 22, name: 'dosya.xls' },
+      ],
+    },
+    {
+      id: 7,
+      name: 'Klasör 3',
+    },
+]
+
