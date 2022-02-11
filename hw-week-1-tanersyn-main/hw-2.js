55da1914735cc50f2a941e3ae159394802b334c6
Taner Sayın <50462952+tanersyn@users.noreply.github.com>
2022-02-11 16:02:18 +0300

    Delete hw-week-1-tanersyn-main directory

--- hw-week-1-tanersyn-main/hw-2.js
+++
@@ -1,6 +0,0 @@
-Array.prototype.includesCI = function (variable) {
-    return this.some((word) => word.toLowerCase() === variable.toLowerCase())
-};
-
-console.log(['kLm', 'Asd', 'deneme',"taner"].includesCI('TanEr'));
-console.log(['asd', 'taner'].includesCI('abc'));
